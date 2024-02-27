7e687fca0d84279c5d2d69a6525eb5a400b19e36
Vinicius <vinicius@pop-os.localdomain>
2024-02-27 18:02:05 -0300

    fix small things

--- src/app/search/[query]/page.tsx
+++ src/app/search/[query]/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import React from 'react'
 
 import API from 'src/services/api'
-import { TheMovieDbResult, TheMovieDbTrendingType } from 'src/services/themoviedb'
+import { TheMovieDbTrendingType } from 'src/services/themoviedb'
 import { Badge } from 'src/ui/badge'
 
 interface PageProps {
@@ -11,25 +11,43 @@ interface PageProps {
     query: string
   }
 }
+
+function parseQueryToString(query: string) {
+  return new URLSearchParams(`?query=${query}`).get('query')
+}
+
 async function SearchPage({ params }: PageProps) {
   const api = API()
   const searchResults = await api.searchAll(params.query)
+  const { moviesCount, seriesCount } = searchResults.reduce((acc: { moviesCount: number, seriesCount: number }, item) => {
+    if (item.is_movie) {
+      return {
+        ...acc,
+        moviesCount: acc.moviesCount + 1,
+      }
+    }
+    return {
+      ...acc,
+      seriesCount: acc.seriesCount + 1,
+    }
+  }, { moviesCount: 0, seriesCount: 0 } as any)
+
   return (
     <div className="px-12 py-4 inline-flex gap-6">
       <div className="hidden md:block max-w-xs h-44 rounded-lg bg-slate-500">
         <div className="bg-slate-900 text-white rounded-t-lg py-3 px-6 text-start">
           <h1 className="font-semibold text-base">Resultados da busca:</h1>
-          <p className="text-sm">"Busca aqui"</p>
+          <p className="text-sm">"{parseQueryToString(params.query)}"</p>
         </div>
         <div className="w-full">
           <ul className="w-full pt-6 px-8">
             <li className="flex justify-between mb-2">
               <p>Filmes</p>
-              <Badge>1</Badge>
+              <Badge>{moviesCount}</Badge>
             </li>
             <li className="flex justify-between">
-              <p>Series</p>
-              <Badge>1</Badge>
+              <p>Séries</p>
+              <Badge>{seriesCount}</Badge>
             </li>
           </ul>
         </div>
@@ -38,7 +56,7 @@ async function SearchPage({ params }: PageProps) {
       <div className="flex-1 h-full">
         {searchResults.map(item => (
           <div className="bg-slate-500 w-full rounded-lg p-4 flex gap-2 mb-4">
-            <Image className="rounded-md" alt="Poster image" src={item.images.poster_paths.sm} width={99} height={140} />
+            <Image className="rounded-md" alt="Poster image" src={item.images.poster_paths.md} width={99} height={140} />
             <div>
               <ItemLink item={item}>
                 <h2 className="text-slate-900 text-base font-semibold">{item.title}</h2>
@@ -61,7 +79,7 @@ interface ItemLinkProps {
 
 function ItemLink({ item, children }: ItemLinkProps) {
   return (
-    <Link href={item.is_movie ? `/movies/${item.id}` : `/tvhsows/${item.id}`}>
+    <Link href={item.is_movie ? `/movie/${item.id}` : `/tvhsow/${item.id}`}>
       {children}
     </Link>
   )
