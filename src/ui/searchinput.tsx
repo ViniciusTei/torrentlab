eb924aa0fe2d075e83b0568f500f270ac47fd742
Vinicius <vinicius@pop-os.localdomain>
2024-02-27 19:24:44 -0300

    feat add search by pressing enter key

--- src/ui/searchinput.tsx
+++ src/ui/searchinput.tsx
@@ -14,16 +14,22 @@ function SearchInput({ onSearch }: Props) {
   async function handleSearch() {
     if (search) {
       onSearch(search)
-      setSearch(undefined)
+      setSearch('')
     }
   }
 
   return (
     <div className="flex items-center justify-center gap-1">
       <Input
+        value={search}
         className="rounded-lg pl-1 text-sm h-12 bg-gray-700 text-white"
         placeholder="Buscar filme"
         onChange={(ev) => setSearch(ev.target.value)}
+        onKeyUp={(ev) => {
+          if (ev.key === "Enter") {
+            handleSearch()
+          }
+        }}
       />
       <Button onClick={handleSearch} className="h-12 w-12 bg-transparent cursor-pointer">
         <FaSearch size="1.1rem" />
