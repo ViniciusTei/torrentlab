3008c3967138b613679a4041c165089eb97868dc
Vinicius <vinicius@pop-os.localdomain>
2024-02-28 06:50:01 -0300

    fix scroll view home page

--- src/components/HomePageList.tsx
+++ src/components/HomePageList.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { useState } from 'react'
-import { TheMovieDbTrendingType } from '../services/themoviedb'
+import { ScrollArea, ScrollBar } from 'src/ui/scroll-area'
 import { TheMovieDbTrendingType } from '../services/types/themoviedb'
 import MovieItem from './MovieItem'
 
@@ -11,16 +10,19 @@ interface Props {
 }
 
 function HomePageList({ title, data }: Props) {
-  const [displayData, setDisplayData] = useState(data.splice(0, 7))
-  console.log({ data })
+  const displayData = data
+
   return (
-    <div className="w-full max-w-full overflow-hidden min-h-fit">
+    <div className="w-full max-w-full overflow-hidden min-h-fit px-4">
       <h2 className="text-3xl capitalize mb-4 mt-6">{title}</h2>
-      <div className="flex items-center gap-2 w-full">
-        {displayData && displayData.length > 0 && displayData.map(movie => (
-          <MovieItem item={movie} />
-        ))}
-      </div>
+      <ScrollArea className="w-full whitespace-nowrap py-4" >
+        <div className="flex items-center gap-2 w-full">
+          {displayData && displayData.length > 0 && displayData.map(movie => (
+            <MovieItem item={movie} />
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </div>
   )
 }
