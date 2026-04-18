295ede805ae5b43df8db05f93df85af7d49bcaa4
Vinicius Teixeira <vinicius.teixeira@cbrdoc.com.br>
2026-04-18 09:42:09 -0300

    refactor: update useServices to use api.fetchTorrents

--- src/services/useServices.ts
+++ src/services/useServices.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import getAPI from './api'
-import Torrents, { JackettItem } from './torrents'
+import { JackettItem } from './torrents'
 import { TheMovieDbDetailsType } from './types/themoviedb'
 
 export type MovieDetails = {
@@ -15,26 +15,20 @@ export default function useServices() {
     return useQuery({
       queryKey: ['home'],
       queryFn: async () => {
-        const responseAllTrending = await api.fetchAllTrending()
-        const responseMovies = await api.fetchTrendingMovies()
-        const responseTvShows = await api.fetchTrendingTvShows()
-
-        return {
-          trending: responseAllTrending,
-          movies: responseMovies,
-          tvShows: responseTvShows,
-        }
+        const [trending, movies, tvShows] = await Promise.all([
+          api.fetchAllTrending(),
+          api.fetchTrendingMovies(),
+          api.fetchTrendingTvShows(),
+        ])
+        return { trending, movies, tvShows }
       }
     })
   }
 
   function useAllTrending() {
     return useQuery({
-      queryKey: ['trending-movies'],
-      queryFn: async () => {
-        const response = await api.fetchAllTrending()
-        return response
-      }
+      queryKey: ['trending-all'],
+      queryFn: () => api.fetchAllTrending(),
     })
   }
 
@@ -42,16 +36,9 @@ export default function useServices() {
     return useQuery({
       queryKey: ['movie-detail', movie],
       queryFn: async () => {
-        try {
-          const results = await api.fetchMovieDetails(movie)
-          const downloads = await Torrents({ type: "movie", imdb_id: results.imdb_id })
-          return {
-            movies: results,
-            downloads
-          } as MovieDetails
-        } catch (error) {
-          throw error
-        }
+        const results = await api.fetchMovieDetails(movie)
+        const downloads = results.imdb_id ? await api.fetchTorrents(results.imdb_id) : []
+        return { movies: results, downloads } as MovieDetails
       },
     })
   }
